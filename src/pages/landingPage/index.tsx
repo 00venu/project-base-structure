427a2b4278bd1c664d32b0f524428d10e55fa395
venu <00venu@gmail.com>
2021-10-03 11:14:32 +0530

    modified

--- src/pages/landingPage/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "../../components/header/Header";
-export { default as LeftNav } from "../../components/nav/leftNav/LeftNav";
-export { default as MinimizedPanel } from "../../components/panels/serviceNotificationPanel/MinimizedPanel";
-export { default as MaximizedPanel } from "../../components/panels/serviceNotificationPanel/MaximizedPanel";
-export { default as MinimizedAlaramPanel } from "../../components/panels/alaramPanel/MinimizedAlaramPanel";
-export { classNames } from "./classess";
